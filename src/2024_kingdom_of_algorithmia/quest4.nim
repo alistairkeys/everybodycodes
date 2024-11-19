c16af8e782242506359b31cbc0db2ad16714f96f
alistairkeys <alistair_keys@hotmail.com>
2024-11-19 09:10:23 +0000

    Quest 4, part 3

--- src/2024_kingdom_of_algorithmia/quest4.nim
+++ src/2024_kingdom_of_algorithmia/quest4.nim
@@ -1,9 +1,10 @@
 # https://everybody.codes/event/2024/quests/3
 
-import std/[sequtils, strutils]
+import std/[algorithm, sequtils, strutils]
 
 proc parseInputFile(filename: string): seq[int] =
-  filename.lines.toSeq.mapIt(parseInt it)
+  result = filename.lines.toSeq.mapIt(parseInt it)
+  result.sort
 
 func part1(sizes: openArray[int]): int =
   let min = min(sizes)
@@ -12,7 +13,14 @@ func part1(sizes: openArray[int]): int =
 func part2(sizes: openArray[int]): int =
   part1(sizes)
 
+func part3(sizes: openArray[int]): int =
+  let avg = sizes[sizes.len div 2]
+  return foldl(sizes, a + abs(b - avg), 0)
+
 echo part1([3,4,7,8]) # example, output is 10
 echo part1(parseInputFile "../input/everybody_codes_e2024_q04_p1.txt")
 
 echo part2(parseInputFile "../input/everybody_codes_e2024_q04_p2.txt")
+
+echo part3(@[2,4,5,6,8]) # example, output is 8
+echo part3(parseInputFile "../input/everybody_codes_e2024_q04_p3.txt")
