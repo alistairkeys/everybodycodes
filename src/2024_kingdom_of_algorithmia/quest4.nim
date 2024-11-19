94f3f0354673de75c9e7d2814a9258daff636432
alistairkeys <alistair_keys@hotmail.com>
2024-11-19 02:47:14 +0000

    Quest 4, part 1

---
+++ src/2024_kingdom_of_algorithmia/quest4.nim
@@ -0,0 +1,13 @@
+# https://everybody.codes/event/2024/quests/3
+
+import std/[sequtils, strutils]
+
+proc parseInputFile(filename: string): seq[int] =
+  filename.lines.toSeq.mapIt(parseInt it)
+
+func part1(sizes: openArray[int]): int =
+  let min = min(sizes)
+  return foldl(sizes, a + b - min, 0)
+  
+echo part1([3,4,7,8]) # example, output is 10
+echo part1(parseInputFile "../input/everybody_codes_e2024_q04_p1.txt")
