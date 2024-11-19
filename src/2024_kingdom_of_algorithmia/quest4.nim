b48808b60fc5b62c20744c5f0fa169d6a31a6c70
alistairkeys <alistair_keys@hotmail.com>
2024-11-19 02:49:49 +0000

    Quest 4,  part 2

--- src/2024_kingdom_of_algorithmia/quest4.nim
+++ src/2024_kingdom_of_algorithmia/quest4.nim
@@ -8,6 +8,11 @@ proc parseInputFile(filename: string): seq[int] =
 func part1(sizes: openArray[int]): int =
   let min = min(sizes)
   return foldl(sizes, a + b - min, 0)
-  
+
+func part2(sizes: openArray[int]): int =
+  part1(sizes)
+
 echo part1([3,4,7,8]) # example, output is 10
-echo part1(parseInputFile "../input/everybody_codes_e2024_q04_p1.txt")
+echo part1(parseInputFile "../input/everybody_codes_e2024_q04_p1.txt") # 82
+
+echo part2(parseInputFile "../input/everybody_codes_e2024_q04_p2.txt")
